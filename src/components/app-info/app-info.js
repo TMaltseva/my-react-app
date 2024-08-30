1dd77debb55cdd8bdd2ad5cd950e9dfd324716a0
TMaltseva <bacardeonie@gmail.com>
2024-08-30 18:40:33 +0300

    feat: add components and forms on classes

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee accounting in company X</h1>
+            <h2>Total number of employees:</h2>
+            <h2>The following employees will receive the bonus:</h2>
+        </div>
+    )
+}
+
+export default AppInfo; 
