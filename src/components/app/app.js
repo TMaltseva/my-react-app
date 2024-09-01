b968ac443a8fdb10edc3b07b39b1077a71a2c0e1
TMaltseva <bacardeonie@gmail.com>
2024-09-01 22:53:49 +0300

    fix: remove duplicate code and add universal function onToggleProp

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -46,22 +46,11 @@ export default class App extends Component {
         });
     }
 
-    onToggleIncrease = (id) => {
+    onToggleProp = (id, prop) => {
         this.setState(({data}) => ({
             data: data.map(item => {
                 if (item.id === id) {
-                    return {...item, increase: !item.increase}
-                }
-                return item;
-            })
-        }))
-    }
-
-    onToggleRise = (id) => {
-        this.setState(({data}) => ({
-            data: data.map(item => {
-                if (item.id === id) {
-                    return {...item, rise: !item.rise}
+                    return {...item, [prop]: !item[prop]}
                 }
                 return item;
             })
@@ -84,8 +73,7 @@ export default class App extends Component {
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem}
-                    onToggleIncrease={this.onToggleIncrease}
-                    onToggleRise={this.onToggleRise} />
+                    onToggleProp={this.onToggleProp} />
                 <EmployeesAddForm onAdd={this.addItem}/>
     
             </div>
